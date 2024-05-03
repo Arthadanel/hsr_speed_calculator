d1f076d76a518d13fe851c0ed46550a08d673229
Arthadanel <kitekate2000cat@gmail.com>
2024-05-03 15:23:22 +0300

    Base layout done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>    
+      <div className="input-data">
+        <TeamData />
+        <CyclesData />
+      </div>
+      <button className='calculate-btn'>Calculate</button>
+      <Timeline />
+    </>
   );
+}
+
+function TeamData() {
+  return (
+    <div className='team-data'>
+      <CharacterData character='1'/>
+      <CharacterData character='2'/>
+      <CharacterData character='3'/>
+      <CharacterData character='4'/>
+    </div>
+  )
+}
+
+function CharacterData({ character }) {
+  return (
+    <div className='character-data'>
+      <div className='character-icon'>{character}</div>
+      <SpeedInput />
+    </div>
+  )
+}
+
+function SpeedInput() {
+  return (
+    <input className='speed-input'></input>
+  )
+}
+
+function CyclesData() {
+  return (
+    <div className='cycle-data'>
+      <label>Cycles:</label>
+      <input className='cycle-input'></input>
+    </div>
+  )
+}
+
+function Timeline() {
+  return (
+    <div className='timeline'>
+      <CycleDivider turnNumber='1'/>
+      <TurnData character='1' actionValue={63}/>
+      <TurnData />
+      <CycleDivider turnNumber='2'/>
+    </div>
+  )
+}
+
+function TurnData({ character, actionValue }) { //character icon + turn value
+  return (
+    <>
+      <div className='turn-data'>{character}</div>
+      <div className='action-value'> {actionValue} </div>
+    </>
+  )
+}
+
+function CycleDivider({ turnNumber }) {
+  return (
+    <>
+      <p>Cycle {turnNumber}</p>
+      <hr/>
+    </>
+  )
 }
 
 export default App;
