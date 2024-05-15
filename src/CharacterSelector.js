9b68b20a4e28392dcc91feec267f48d796f76063
Arthadanel <kitekate2000cat@gmail.com>
2024-05-15 17:45:44 +0300

    finish clear selection button

--- src/CharacterSelector.js
+++ src/CharacterSelector.js
@@ -3,58 +3,74 @@ import { ConstantsList } from './Constants';
 
 export function CharacterSelector({characterData, team, setTeam, speedValues, setSpeedValues}) {
     const [lastPosition, setLastPosition] = useState(0);
+    const [listingSelectionReferences, setListingSelectionReferences] = useState(Array(ConstantsList.TEAM_SIZE).fill(ConstantsList.EMPTY_CHARACTER));
+
     function SelectCharacter(characterData, selected, setSelected) { //todo: move to character selector
         console.log(selected, characterData);
         if(!selected && lastPosition >= ConstantsList.TEAM_SIZE) return; //all teamslots filled
         if (!selected) {
-        let newTeam = [...team];
-        newTeam[lastPosition] = characterData;
-        let newSpeed = [...speedValues]
-        newSpeed[lastPosition] = characterData["base_speed"];
-        let newPosition = ConstantsList.TEAM_SIZE;
-        for (let i = 0; i < ConstantsList.TEAM_SIZE; i++) {
-            if (newTeam[i] === ConstantsList.EMPTY_CHARACTER) {
-            newPosition = i;
-            break;
+            let newTeam = [...team];
+            newTeam[lastPosition] = characterData;
+            let newSpeed = [...speedValues]
+            newSpeed[lastPosition] = characterData["base_speed"];
+            let newPosition = ConstantsList.TEAM_SIZE;
+            for (let i = 0; i < ConstantsList.TEAM_SIZE; i++) {
+                if (newTeam[i] === ConstantsList.EMPTY_CHARACTER) {
+                newPosition = i;
+                break;
+                }
             }
-        }
-        setSpeedValues(newSpeed);
-        setTeam(newTeam);
-        setLastPosition(newPosition);
-        setSelected(!selected);
+            let newListings = [...listingSelectionReferences];
+            newListings[lastPosition] = setSelected;
+            setListingSelectionReferences(newListings);
+            setSpeedValues(newSpeed);
+            setTeam(newTeam);
+            setSelected(!selected);
+            setLastPosition(newPosition);
+
         } else if (selected) {
-        console.log('selected');
-        let position = 4;
-        for (let i = 0; i < ConstantsList.TEAM_SIZE; i++) {
-            if (team[i] === characterData) {
-            position = i;
-            break;
+            console.log('selected');
+            let position = 4;
+            for (let i = 0; i < ConstantsList.TEAM_SIZE; i++) {
+                if (team[i] === characterData) {
+                position = i;
+                break;
+                }
             }
-        }
-        if (position < lastPosition) setLastPosition(position);
-        let newTeam = [...team];
-        newTeam[position] = ConstantsList.EMPTY_CHARACTER;
-        let newSpeed = [...speedValues]
-        newSpeed[position] = ConstantsList.DEFAULT_SPEED;
-        setSpeedValues(newSpeed);
-        setTeam(newTeam);
-        setSelected(!selected);
+            if (position < lastPosition) setLastPosition(position);
+            let newTeam = [...team];
+            newTeam[position] = ConstantsList.EMPTY_CHARACTER;
+            let newSpeed = [...speedValues]
+            newSpeed[position] = ConstantsList.DEFAULT_SPEED;
+            let newListings = [...listingSelectionReferences];
+            newListings[lastPosition] = ConstantsList.EMPTY_CHARACTER;
+            setListingSelectionReferences(newListings);
+            setSpeedValues(newSpeed);
+            setTeam(newTeam);
+            setSelected(!selected);
         }
         // setLastPosition(lastPosition)
     }
 
-  function Clear() {  //todo: move to character selector
-    //todo: clear data in character listing
-    setLastPosition(0);
-    setTeam(Array(ConstantsList.TEAM_SIZE).fill(ConstantsList.EMPTY_CHARACTER));
-    setSpeedValues(Array(ConstantsList.TEAM_SIZE).fill(ConstantsList.DEFAULT_SPEED));
-  }
-
   let characters = [];
   for (let i = 0; i < characterData.length; i++) {
     const character = characterData[i];
-    characters.push(<CharacterListing key={character.name} index={i} data={character} onCharacterClick={SelectCharacter}/>);
+    characters.push(<CharacterListing key={character.name} index={i} data={character} onCharacterClick={SelectCharacter} />);
   }
+
+  function Clear() {  //todo: move to character selector
+    //todo: clear data in character listing
+    console.log("Listings: ", listingSelectionReferences);
+    for (let i = 0; i < ConstantsList.TEAM_SIZE; i++) {
+        const listing = listingSelectionReferences[i];
+        if (listing !== ConstantsList.EMPTY_CHARACTER) listing(false);
+    }
+    setLastPosition(0);
+    setTeam(Array(ConstantsList.TEAM_SIZE).fill(ConstantsList.EMPTY_CHARACTER));
+    setSpeedValues(Array(ConstantsList.TEAM_SIZE).fill(ConstantsList.DEFAULT_SPEED));
+    setListingSelectionReferences(Array(ConstantsList.TEAM_SIZE).fill(ConstantsList.EMPTY_CHARACTER));
+  }
+
   return (
     <>    
         <button className='clear-btn' onClick={Clear}>Clear</button>
@@ -65,11 +81,11 @@ export function CharacterSelector({characterData, team, setTeam, speedValues, se
   )
 }
 
-function CharacterListing({ data, onCharacterClick, index }) {
+function CharacterListing({ data, onCharacterClick }) {
   const [selected, setSelected] = useState(false);
   return (    
     <div className={selected ? 'listing-border' : 'listing-borderless'}>
-      <img className='character-icon' src={process.env.PUBLIC_URL + data.icon} alt={data.name} onClick={() => onCharacterClick(data, selected, setSelected, index)}/>
+      <img className='character-icon' src={process.env.PUBLIC_URL + data.icon} alt={data.name} onClick={() => onCharacterClick(data, selected, setSelected)}/>
     </div>
   )
 }
